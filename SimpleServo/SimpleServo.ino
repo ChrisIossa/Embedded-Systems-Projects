2849c2fd9746ac7cf8f8c5bdca28340373017b60
Chris Iossa <Chris.Iossa@gmail.com>
2017-10-16 15:21:37 -0400

    Update SimpleServo

--- SimpleServo/SimpleServo.ino
+++ SimpleServo/SimpleServo.ino
@@ -46,12 +46,12 @@ void loop()
   for(int i = 0; i < 180; i += 1 )
   {
     myservo.write( i );
-    delay( 10 );
+   // delay( 10 );
   }
   for( int i = 180; i > 0; i -= 1 )
   {
     myservo.write( i );
-    delay( 10 );
+   delay( 10 );
   }
   //*/
 }
