cdc1ca37f9a0e2bebd8efdccee866ae6918b7744
Chris Iossa <Chris.Iossa@gmail.com>
2017-10-24 14:56:52 -0400

    Added altBlink to work with ISR

---
+++ altBlink/altBlink.ino
@@ -0,0 +1,48 @@
+
+
+#define btn 2
+volatile byte fastLED=5;
+volatile byte slowLED=6;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(fastLED,OUTPUT);
+  pinMode(slowLED, OUTPUT);
+  pinMode(btn, INPUT_PULLUP);
+  attachInterrupt(0,flipLED_ISR, CHANGE);
+  
+}
+
+
+
+void flipLED_ISR()
+{
+  byte temp = slowLED;
+  slowLED=fastLED;
+  fastLED =temp;
+  
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(fastLED,HIGH);
+  digitalWrite(slowLED,HIGH);
+  
+  delay(24);
+  digitalWrite(fastLED,LOW);
+  
+  
+  delay(240);
+  digitalWrite(slowLED,LOW);
+  digitalWrite(fastLED,HIGH);
+ 
+  delay(240);
+  digitalWrite(fastLED,LOW);
+  digitalWrite(slowLED,LOW);
+ 
+  
+  delay(240);
+ 
+  
+}
